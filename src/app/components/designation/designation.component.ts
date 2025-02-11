63cf332d2ba4b4ef3c4953cd76479dc1fd5e7f7a
amankumar-am <aman.kumar@aditmicrosys.com>
2025-02-11 11:54:35 +0530

    fixed commit error. next to push

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { APIResponseModel, IDesignation } from '../../model/interface/role';
 
 @Component({
   selector: 'app-designation',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.css'
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit {
 
+  designationList: IDesignation[] = [];
+  masterService = inject(MasterService);
+  isLoader: boolean = true;
+
+  ngOnInit(): void {
+    this.masterService.getDesignations().subscribe((result: APIResponseModel) => {
+      this.designationList = result.data;
+      this.isLoader = false;
+    }, error => {
+      alert("API Error/ Network Down")
+      this.isLoader = true;
+    })
+  }
 }
